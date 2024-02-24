9a04bc6d185c6800a39dc42de6edd470e7d261b3
enesmezanii <enesmezani1@gmail.com>
2024-02-24 20:06:14 +0100

    from here fix

--- warehouse.py
+++ warehouse.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import datetime  # Import datetime module
 
 source_db_config = {
     'host': 'localhost',
@@ -37,34 +38,13 @@ def transform_and_load_to_target(data, table_name):
     try:
         target_cursor.execute("START TRANSACTION")
 
-        # if table_name == 'companies':
-        #     for row in data:
-        #         target_cursor.execute("INSERT INTO datawarehouse.dimclient (id, company_name) VALUES (%s, %s)", (row[0], row[1],))
-        # elif table_name == 'products':
-        #     for row in data:
-        #         target_cursor.execute("INSERT INTO datawarehouse.dimproduct (id, category, name, family) VALUES (%s, %s, %s, %s)", (row[0], row[3], row[2], row[1],))
-        if table_name == 'countries':
-            for row in data:
-                country_id, country_name = row[0], row[1]
-
-                # Join countries and regions to get region information
-                source_connection = mysql.connector.connect(**source_db_config)
-                source_cursor = source_connection.cursor()
-                join_query = f"""
-                    SELECT countries.id, countries.name, regions.name
-                    FROM transportation.countries
-                    LEFT JOIN transportation.regions ON countries.id = regions.country_id
-                    WHERE countries.id = {country_id}
-                """
-                source_cursor.execute(join_query)
-                result = source_cursor.fetchone()
-                source_cursor.close()
-
-                # Extract values from the joined result
-                region_id, region_name = result[0], result[2]
-
-                target_cursor.execute("INSERT INTO datawarehouse.dimcountry (id, name, region) VALUES (%s, %s, %s)", (country_id, country_name, region_name))
-
+        for row in data:
+            # if table_name == 'companies':
+            #     target_cursor.execute("INSERT INTO datawarehouse.dimclient (id, company_name) VALUES (%s, %s)", (row[0], row[1],))
+            # elif table_name == 'products':
+            #     target_cursor.execute("INSERT INTO datawarehouse.dimproduct (id, category, name, family) VALUES (%s, %s, %s, %s)", (row[0], row[3], row[2],row[1], ))
+            if table_name == 'countries':
+                target_cursor.execute("INSERT INTO datawarehouse.dimcountry (id, name, region) VALUES (%s, %s, %s)", (row[0], row[1], row[2],))
         target_cursor.execute("COMMIT")
 
     except Exception as e:
