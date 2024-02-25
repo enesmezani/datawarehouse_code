2008d12f5acb191ee4da16f8c403cc51327f82c9
enesmezanii <enesmezani1@gmail.com>
2024-02-25 19:51:09 +0100

    some fixies for datawarehouse

--- warehouse.py
+++ warehouse.py
@@ -39,11 +39,34 @@ def transform_and_load_to_target(data, table_name):
         target_cursor.execute("START TRANSACTION")
 
         for row in data:
-            # if table_name == 'companies':
-            #     target_cursor.execute("INSERT INTO datawarehouse.dimclient (id, company_name) VALUES (%s, %s)", (row[0], row[1],))
-            # elif table_name == 'products':
-            #     target_cursor.execute("INSERT INTO datawarehouse.dimproduct (id, category, name, family) VALUES (%s, %s, %s, %s)", (row[0], row[3], row[2],row[1], ))
-            if table_name == 'countries':
+            if table_name == 'companies':
+                target_cursor.execute("INSERT INTO datawarehouse.dimclient (id, company_name) VALUES (%s, %s)", (row[0], row[1],))
+                if row[1] == 'Company':
+                    company_type = 'electric_company'
+                    industry_id = row[0]
+                elif row[1] == 'Easttest':
+                    company_type = 'mechanic_company'
+                    industry_id = row[0]
+                elif row[1] == 'test company 2':
+                    company_type = 'industry'
+                    industry_id = row[0]
+                target_cursor.execute("INSERT INTO datawarehouse.dimclient_companytype (company_type, industry_id) VALUES (%s, %s)", (company_type,industry_id,))      
+            elif table_name == 'products':
+                target_cursor.execute("INSERT INTO datawarehouse.dimproduct (id, category, code, family) VALUES (%s, %s, %s, %s)", (row[0], row[3], row[2], row[1]))
+
+                category_name = row[3]
+
+                target_cursor.execute("SELECT id FROM datawarehouse.dimproduct_subcategory WHERE subcategory = %s", (category_name,))
+                existing_category = target_cursor.fetchone()
+
+                if not existing_category:
+                    target_cursor.execute("INSERT INTO datawarehouse.dimproduct_subcategory (subcategory) VALUES (%s)", (category_name,))
+
+                target_cursor.execute("SELECT id FROM datawarehouse.dimproduct_subcategory WHERE subcategory = %s", (category_name,))
+                category_id = target_cursor.fetchone()[0]
+
+                target_cursor.execute("UPDATE datawarehouse.dimproduct SET category_id = %s WHERE id = %s", (category_id, row[0]))
+            elif table_name == 'countries':
                 target_cursor.execute("INSERT INTO datawarehouse.dimcountry (id, name, code) VALUES (%s, %s, %s)", (row[0], row[1], row[2],))
             elif table_name =='regions':
                 target_cursor.execute("INSERT INTO datawarehouse.dimcountry_subregion (id, subregion, country_id) VALUES (%s, %s, %s)", (row[0], row[1], row[2]))
